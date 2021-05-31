cad019b693b8954b901523252d4eeb45029e7443
Gleb Berliand <gberliand@gmail.com>
2021-05-31 16:16:18 +0300

    5 step changes

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/GlebCoder/python-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/GlebCoder/python-project-lvl1/actions)
 
 https://github.com/GlebCoder/python-project-lvl1/actions/workflows/lint-check.yml/badge.svg
+
+https://asciinema.org/a/0z95rvcKEHCMSiFM4jaYn7WMP - a video recoding of the brain-even game
---
+++ brain_games/even_or_not.py
@@ -0,0 +1,18 @@
+import prompt
+from random import randint
+def even_or_not():
+    print("Welcome to the Brain games!")
+    name = prompt.string("May I have your name? ")
+    print("Hello, {}".format(name))
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    i = 1
+    while i <= 3:
+        value = randint(1, 1000)
+        print('Question: {}'.format(value))
+        answer = prompt.string("Your answer: ")
+        if (value % 2 == 0 and answer.lower() == "no") or (value % 2 == 1 and answer.lower() == "yes"):
+            return print("Let's try again, {}!".format(name))
+        else:
+            print("Correct!")
+            i += 1
+    return print("Congratulations, {}!".format(name))
