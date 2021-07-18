ebc9862c6dc2527e318d65b76a0ac01129dfa8af
Gleb Berliand <gberliand@gmail.com>
2021-07-18 16:43:12 +0300

    changes to prime-game.py file

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -3,16 +3,14 @@ from random import randint
 
 
 GAME_CONDITION = 'Answer "yes" if the number is prime, otherwise answer "no".'
-START_OF_RANGE = 1
-END_OF_RANGE = 100
+MIN_NUM = 1
+MAX_NUM = 100
 
 
 def is_prime(number):
     if number > 1:
         for index in range(2, number // 2 + 1):
-            if number % index != 0:
-                continue
-            else:
+            if number % index == 0:
                 return False
         return True
     else:
@@ -20,7 +18,7 @@ def is_prime(number):
 
 
 def generate_question_and_answer():
-    question = randint(START_OF_RANGE, END_OF_RANGE)
+    question = randint(MIN_NUM, MAX_NUM)
     if is_prime(question):
         answer = 'yes'
     else:
