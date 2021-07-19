6c94da882bccaf527a093ec74a49537ec46e72f5
Gleb Berliand <gberliand@gmail.com>
2021-07-19 14:29:06 +0300

    correction for "goodbye" options

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -5,12 +5,7 @@ import prompt
 MAX_NUN_OF_QUESTIONS = 3  # Maximal number of questions in brain-games
 
 
-def print_goodbye_option(user_answer, right_answer):
-    print(f"'{user_answer}' is wrong answer ;(.", end=" ")
-    print(f"Correct answer was '{right_answer}'")
-
-
-def run_game(game_condition, question_and_answer, goodbye_opt=0):
+def run_game(game_condition, question_and_answer):
     print("Welcome to the Brain games!")
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}")
@@ -21,9 +16,8 @@ def run_game(game_condition, question_and_answer, goodbye_opt=0):
         user_answer = prompt.string('Your answer: ')
         if right_answer == user_answer.lower():
             print('Correct!')
-        elif goodbye_opt == 1:
-            print_goodbye_option(user_answer, right_answer)
-            return print(f"Let's try again, {name}!")
         else:
+            print(f"'{user_answer}' is wrong answer ;(.", end=" ")
+            print(f"Correct answer was '{right_answer}'")
             return print(f"Let's try again, {name}!")
     return print(f"Congratulations, {name}!")
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -6,7 +6,7 @@ from brain_games import engine
 def main():
     GAME_CONDITION = calc.GAME_CONDITION
     question_and_answer = calc.generate_question_and_answer
-    engine.run_game(GAME_CONDITION, question_and_answer, goodbye_opt=1)
+    engine.run_game(GAME_CONDITION, question_and_answer)
 
 
 if __name__ == "__main__":
